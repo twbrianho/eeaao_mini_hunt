b731b6b19666fa774f4c721b73b4b0c4c53b0fc7
Brian Ho <brianslho@gmail.com>
2023-10-30 03:30:43 -0700

    local storage

--- src/utils/atoms.ts
+++ src/utils/atoms.ts
@@ -1,7 +1,13 @@
-import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 
-export const taxFormsSolvedAtom = atom(false);
-export const veryBusySolvedAtom = atom(false);
-export const verseJumpingSolvedAtom = atom(false);
-export const raccoontouilleSolvedAtom = atom(false);
-export const metaSolvedAtom = atom(false);
+export const taxFormsSolvedAtom = atomWithStorage("taxFormsSolvedAtom", false);
+export const veryBusySolvedAtom = atomWithStorage("veryBusySolvedAtom", false);
+export const verseJumpingSolvedAtom = atomWithStorage(
+  "verseJumpingSolvedAtom",
+  false,
+);
+export const raccoontouilleSolvedAtom = atomWithStorage(
+  "raccoontouilleSolvedAtom",
+  false,
+);
+export const metaSolvedAtom = atomWithStorage("metaSolvedAtom", false);
